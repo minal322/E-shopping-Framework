cb4101e42086dcc726fd8c4fad037ddb7015e10f
minal322 <95573522+minal322@users.noreply.github.com>
2025-04-02 17:54:47 +0530

    Commented screenshot for error in  conftest.py

--- src/conftest.py
+++ src/conftest.py
@@ -27,32 +27,32 @@ def browserInstance(request):
     #below yield works as a teardown statements
     driver.close()
 
-#this hook is automatically called when test fails and takes screenshot
-@pytest.hookimpl( hookwrapper=True )
-def pytest_runtest_makereport(item):
-    """
-        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-        :param item:
-        """
-    pytest_html = item.config.pluginmanager.getplugin( 'html' )
-    outcome = yield
-    report = outcome.get_result()
-    extra = getattr( report, 'extra', [] )
+# #this hook is automatically called when test fails and takes screenshot
+# @pytest.hookimpl( hookwrapper=True )
+# def pytest_runtest_makereport(item):
+#     """
+#         Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+#         :param item:
+#         """
+#     pytest_html = item.config.pluginmanager.getplugin( 'html' )
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr( report, 'extra', [] )
 
-    if report.when == 'call' or report.when == "setup":
-        xfail = hasattr( report, 'wasxfail' )
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            reports_dir = os.path.join( os.path.dirname( __file__ ), 'reports' )
-            file_name = os.path.join( reports_dir, report.nodeid.replace( "::", "_" ) + ".png" )
-            print( "file name is " + file_name )
-            _capture_screenshot( file_name )
-            if file_name:
-                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
-                extra.append( pytest_html.extras.html( html ) )
-        report.extras = extra
+#     if report.when == 'call' or report.when == "setup":
+#         xfail = hasattr( report, 'wasxfail' )
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             reports_dir = os.path.join( os.path.dirname( __file__ ), 'reports' )
+#             file_name = os.path.join( reports_dir, report.nodeid.replace( "::", "_" ) + ".png" )
+#             print( "file name is " + file_name )
+#             _capture_screenshot( file_name )
+#             if file_name:
+#                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+#                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
+#                 extra.append( pytest_html.extras.html( html ) )
+#         report.extras = extra
 
 
-def _capture_screenshot(file_name):
-    driver.get_screenshot_as_file(file_name)
+# def _capture_screenshot(file_name):
+#     driver.get_screenshot_as_file(file_name)
 
